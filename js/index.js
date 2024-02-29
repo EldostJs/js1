49d3c3640024f58a22e6172ed537f34cd68384c4
unknown <mirzeyev0308@gamil.com>
2024-02-29 11:52:08 +0400

    1221

---
+++ js/index.js
@@ -0,0 +1,38 @@
+// console.log('Hello World');
+// window.alert('This is a My Js')
+// document.getElementById("myH").textContent = 'Hello My Friend'
+// var input1 = prompt('What Your Name')
+// var input = prompt('What Your Surname')
+// var confirm = confirm('Are you programmer?')
+// var num1 = prompt(`Write down your birthday`)
+// var sum = 2024 - num1
+// alert(sum)
+// console.log(input1 , input , confirm ,sum);
+
+// var name = `Eldost`
+// var pizzaName = `3 sisters,`
+// var pizzaPrice = `total cost is 5$`
+// var pizzaCount = `has bought 3 pizzas,`
+
+// console.log(name, pizzaCount, pizzaName, pizzaPrice);
+// var confirm = confirm(`Do you like sushi`)
+// var prompt = prompt(` What kind  of sushi do you like `)
+// alert(prompt, confirm)
+
+// console.log(confirm, prompt);
+// var num1 = prompt('First Numbe');
+// var number1 = parseInt(num1);
+
+// var num2 = prompt('Second Number');
+// var number2 = parseInt(num2);
+
+// var num3 = prompt('3 Number');
+// var number3 = parseInt(num3);
+
+// var sum1 = number1 * number2 * number3;
+// var sum2 = number1 - number2;
+// var sum3 = number1 + number3;
+
+// alert(sum1);
+// alert(sum2);
+// alert(sum3);
